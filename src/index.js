52259b5f1f831405ec41f246d0db4507756f5438
Dikmit02 <dikshamittal21297@gmail.com>
2020-06-03 18:09:19 +0530

    auth of task and user done

--- src/index.js
+++ src/index.js
@@ -10,6 +10,20 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
+const Task = require('./models/task')
+const User = require('./models/user')
+
+const main = async () => {
+    const task = await Task.findById('5ed78f1739eae2768382ac9e')
+    await task.populate('owner').execPopulate()
+    console.log(task.owner)
+
+    // const user = await User.findById('5ed78a4472dc8f6ff0ebda9b')
+    // await user.populate('tasks').execPopulate()
+    // console.log(user.tasks)
+}
+
+main()
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
